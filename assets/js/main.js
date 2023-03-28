218991a201596d99965a195a485bd499b340a5a0
Victor Chibuzor Omerenma <only1victor@yahoo.com>
2023-03-28 15:33:00 +0100

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,43 @@
+/*
+	const hamburger = document.querySelector(".hamburger");
+	const navMenu = document.querySelector(".nav-menu");
+
+	hamburger.addEventListener("click", mobileMenu);
+
+	function mobileMenu() {
+		hamburger.classList.toggle("active");
+		navMenu.classList.toggle("active");
+	}
+
+	// burgerMenu.click(function() {
+	//   this.classList.toggle('open')
+	// })
+
+	// const burgerMenu = document.querySelector('.menu')
+
+	// burgerMenu.addEventListener('click', event => {
+	//   burgerMenu.classList.toggle('open')
+	// }).sideBarNav
+
+*/
+
+const burgerMenu = document.querySelector('.menu')
+const sideNav = document.querySelector('.sideBarNav')
+
+burgerMenu.addEventListener('click', sidemenu)
+function sidemenu() {
+  burgerMenu.classList.toggle('open')
+  sideNav.classList.toggle('active')
+}
+
+// window.addEventListener('mouseup', function(event) {
+//   var box = document.getElementById('sideBarNav')
+//   if (event.target != box && event.target.parentNode != box) {
+//     box.style.display = 'none'
+//   }
+// })
+
+const today = new Date()
+const year = today.getFullYear()
+
+document.getElementById('copyright').innerHTML = year
