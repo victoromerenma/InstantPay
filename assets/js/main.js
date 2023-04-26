4866332ce8e0f9d09c38f385131974dceaa9bcdd
Victor Chibuzor Omerenma <only1victor@yahoo.com>
2023-04-26 09:17:16 +0100

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,7 +22,7 @@ loginButton.addEventListener('click', e => {
   const username = loginForm.email_mobileNumber.value
   const password = loginForm.login_Password.value
 
-  if (username === 'Omolola.Kolade@gmail.com' && password === 'OK_password') {
+  if (username === 'Omolola.Kolade@gmail.com' && password === 'Omo_na_P@ss') {
     //alert('You have successfully logged in.')
     window.location.href = './dashboard.html'
   } else {
