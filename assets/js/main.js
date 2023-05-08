0c967b8b9e4654a1f238939b6470965dae746e43
Victor Chibuzor Omerenma <only1victor@yahoo.com>
2023-05-08 10:05:04 +0100

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,15 +14,26 @@ function sidemenu() {
 //
 
 const loginForm = document.getElementById('loginForm')
+const password = document.querySelector('#login_Password')
+const togglePassword = document.querySelector('#togglePassword')
 const loginButton = document.getElementById('loginForm__Submit')
 const loginErrorMsg = document.getElementById('login-error-msg')
+
+togglePassword.addEventListener('click', function(e) {
+  // toggle the type attribute
+  const type =
+    password.getAttribute('type') === 'password' ? 'text' : 'password'
+  password.setAttribute('type', type)
+  // toggle the eye slash icon
+  this.classList.toggle('bi-eye')
+})
 
 loginButton.addEventListener('click', e => {
   e.preventDefault()
   const username = loginForm.email_mobileNumber.value
   const password = loginForm.login_Password.value
 
-  if (username === 'omolola.kolade@ail.com' && password === 'Pass@123') {
+  if (username === 'omolola.kolade@gmail.com' && password === 'Pass@123') {
     //alert('You have successfully logged in.')
     window.location.href = './dashboard.html'
   } else {
